438a92d47b1efa4d63864c75f701216632542fec
leadgitaccount <harshadachitte30@gmail.com>
2026-09-05 09:43:39 +0530

    pass removed

--- database/read_data.py
+++ database/read_data.py
@@ -15,8 +15,8 @@ from datetime import date
 connection = mysql.connector.connect(
     host='localhost',
     port=3306,
-    user='root',
-    password='CareSync@2024',
+    user='',
+    password='',
     database='caresync'
 )
 cursor = connection.cursor(dictionary=True)  # dictionary=True gives us column names
